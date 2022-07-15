189482814a42c65ad0c986fbee4259cfabe29c91
JuanCarlosAguilarB <abjuancarlos.12@gmail.com>
2022-07-15 01:00:33 -0500

    cambio

--- django_portfolio/settings/base.py
+++ django_portfolio/settings/base.py
@@ -95,10 +95,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = str(BASE_DIR / 'static')
 STATIC_URL = "/static/"
 MEDIA_URL = "/public/"
 MEDIA_ROOT = BASE_DIR / "media"
-
+STATICFILES_DIRS = [(BASE_DIR, 'static'),]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
