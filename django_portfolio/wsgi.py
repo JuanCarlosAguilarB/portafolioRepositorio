818a7d40e7985bf7289bd57839f49ec0cd8f9dd8
JuanCarlosAguilarB <abjuancarlos.12@gmail.com>
2022-07-15 00:49:00 -0500

    cambio

--- django_portfolio/wsgi.py
+++ django_portfolio/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_portfolio.settings.production')
 
 #application = get_wsgi_application()
-from dj_static import clickjacking
+from dj_static import Cling
 
 application = Cling(get_wsgi_application())
 
