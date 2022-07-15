a4de7d95ee70e7c55e728b27110607dcd3dbc055
JuanCarlosAguilarB <abjuancarlos.12@gmail.com>
2022-07-15 13:49:49 -0500

    FIX BUG arreglo createsuperuser

--- django_portfolio/settings/base.py
+++ django_portfolio/settings/base.py
@@ -1,4 +1,4 @@
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -95,11 +95,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = str(BASE_DIR / 'static')
+
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = "/public/"
 MEDIA_ROOT = BASE_DIR / "media"
-STATICFILES_DIRS = [(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = [(BASE_DIR, 'static'),]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- django_portfolio/wsgi.py
+++ django_portfolio/wsgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_portfolio.settings.production')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_portfolio.settings.local')
 
 #application = get_wsgi_application()
 from dj_static import Cling
