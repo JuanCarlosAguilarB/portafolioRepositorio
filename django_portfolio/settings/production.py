13610059119b894763d4dbaa8737b916f4c8970a
JuanCarlosAguilarB <abjuancarlos.12@gmail.com>
2022-07-15 00:16:29 -0500

    se agrega base de datos a produccion

--- django_portfolio/settings/production.py
+++ django_portfolio/settings/production.py
@@ -8,8 +8,12 @@ ALLOWED_HOSTS = ['djangoportfoliojuan.herokuapp.com']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'datigg9juk0hvc',
+        'USER': 'kurdoxgwwwwgst',
+        'PASSWORD': '709b9ede5dd8d7ab7020f326ba409f095f1ba85242e536daf8d6466103971f6e',
+        'HOST': 'ec2-52-205-61-230.compute-1.amazonaws.com',
+        'PORT': '',
     }
 }
